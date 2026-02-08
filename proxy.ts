a96911ac9a06d86e4cebc5d0b0c03a23aa012a85
Franck Le Pichon <franckiiboss@gmail.com>
2026-02-08 20:32:30 +0100

    Add proxy middleware for authentication redirection

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 const ACCESS_TOKEN_COOKIE = "wh_access_token";
-export function middleware(req: NextRequest): NextResponse | undefined {
+export function proxy(req: NextRequest): NextResponse | undefined {
   const { pathname } = req.nextUrl;
   const hasToken = !!req.cookies.get(ACCESS_TOKEN_COOKIE)?.value;
   if (pathname.startsWith("/app")) {
